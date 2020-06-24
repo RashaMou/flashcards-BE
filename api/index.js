a69649caebee9af7c661dfc5b08b1754799704d3
Rasha Moumneh <rasha@fastmail.com>
2020-06-23 20:37:41 -0400

    Fix register user function

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 // public routes
-router.use("/users", require("./users"));
+router.use('/users', require('./users'));
+router.use('/auth', require('./auth'));
 
 // private routes
-router.use("/decks", require("./decks"));
+router.use('/decks', require('./decks'));
 // router.use("/cards", require("./cards"));
 
 module.exports = router;
--- helpers/signToken.js
+++ helpers/signToken.js
@@ -1,15 +1,15 @@
-const configSecret = require("../config/secret");
-const jwt = require("jsonwebtoken");
+const configSecret = require('../config/secret');
+const jwt = require('jsonwebtoken');
 
 module.exports = (user) => {
   const payload = {
-    username: user.username,
+    name: user.name,
   };
 
   const secret = configSecret.jwtSecret;
 
   const options = {
-    expiresIn: "24h",
+    expiresIn: '24h',
   };
 
   return jwt.sign(payload, secret, options);
